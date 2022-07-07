30fff6654f6c7856c0f7aec134219cfcc432a87a
oak45985 <oak45985@gmail.com>
2022-07-06 22:53:36 -0500

    all images going up, local storage started

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
 var userFormEl = document.querySelector("#user-form");
-var cityInputEl = document.querySelector("#city-name")
-var stateInputEl = document.querySelector("#city-initials")
-var currentContainerEl = document.querySelector("#current-display")
-var dayZeroContainerEl = document.querySelector("#future-display-one")
-var dayOneContainerEl = document.querySelector("#future-display-two")
-var dayTwoContainerEl = document.querySelector("#future-display-three")
-var dayThreeContainerEl = document.querySelector("#future-display-four")
-var dayFourContainerEl = document.querySelector("#future-display-five")
+var cityInputEl = document.querySelector("#city-name");
+var stateInputEl = document.querySelector("#city-initials");
+var currentContainerEl = document.querySelector("#current-display");
+var dayZeroContainerEl = document.querySelector("#future-display-one");
+var dayOneContainerEl = document.querySelector("#future-display-two");
+var dayTwoContainerEl = document.querySelector("#future-display-three");
+var dayThreeContainerEl = document.querySelector("#future-display-four");
+var dayFourContainerEl = document.querySelector("#future-display-five");
+var btnListEl = document.querySelector("#btn-list");
 var currentDay = [];
 var futureDays = [];
 
@@ -18,14 +19,10 @@ var getCityState = function(event) {
 
     if (cityName, stateInit) {
         getLatLong(cityName, stateInit);
-        // function to store (cityName) + (stateInit);
         cityInputEl.value = "";
         stateInputEl.value = "";
-        // console.log(cityName + " is the city you've input. " + stateInit + " is the state you've input.");
-        // console.log("shit worked!");
     } else {
         alert("Please enter a City and ST.");
-        // console.log("c'mon fucker");
     }
     
 }
@@ -40,12 +37,28 @@ var getLatLong = function(cityName, stateInit) {
                 lat = data[0].lat;
                 lon = data[0].lon;
                 getWeather(lat, lon, cityName, stateInit);
+                createButton(lat, lon, cityName, stateInit);
             });
         }  else {
             console.log("something aint right");
         }
     });
 };
+
+var createButton = function(lat, lon, cityName, stateInit) {
+
+    var cityBtnEl = document.createElement("btn");
+    cityBtnEl.classList = "btn btn-secondary";
+    cityBtnEl.textContent = cityName + ", " + stateInit;
+    btnListEl.appendChild(cityBtnEl);
+
+    var cityStorage = {
+        latitude: lat,
+        longitude: lon,
+    }
+    localStorage.setItem("cityStorage", JSON.stringify(cityStorage));
+
+}
 
 var getWeather = function(lat, lon, cityName, stateInit) {
 
@@ -81,17 +94,14 @@ var displayCurrentDay = function(currentDay, cityName, stateInit) {
         "http://openweathermap.org/img/wn/" + currentDay.current.weather[0].icon + "@2x.png"
     );
 
-    console.log(currentDay.current.weather[0]);
-
-    currentContainerEl.appendChild(image);
+    currentDayNameEl.appendChild(image);
 
     var currentDayInfoEl = document.createElement("p");
     currentDayInfoEl.classList = "card-title text-dark";
     currentDayInfoEl.innerHTML = "<p class='card-text text-dark'>" 
     + currentDay.current.temp + "°F<br>" 
     + currentDay.current.wind_speed + " MPH<br>"
-    + currentDay.current.humidity + "%<br>"
-    + "UV Index: "+ currentDay.current.uvi + "</p>";
+    + currentDay.current.humidity + "%</p>";
     currentContainerEl.appendChild(currentDayInfoEl);
 
     var uvIndicatorEl = document.createElement("p")
@@ -105,21 +115,15 @@ var displayCurrentDay = function(currentDay, cityName, stateInit) {
     uvIndicatorEl.innerHTML = "<span class='pb-2 align-middle'>UV Index: " + currentDay.current.uvi + "</span>";
     // uvIndicatorEl.textContent = "UV Index: " + currentDay.current.uvi;
     currentContainerEl.appendChild(uvIndicatorEl);
-
-
-    // // var convertTemp = (1.8 * (currentDay.temp - 273)) + 32;
-    // console.log(currentDay.temp + "°F");
-    // // var convertWind = currentDay.wind_speed * 2.237;
-    // console.log(currentDay.wind_speed + " MPH");
-    // console.log(currentDay.humidity + "%");
-    // console.log(currentDay.uvi);
 }
 
 var displayFutureDays = function(futureDays) {
 
     var futureDayInfoEl = document.createElement("p");
     futureDayInfoEl.classList = "card-title text-dark";
-    futureDayInfoEl.innerHTML = "<h5 class='card-title text-dark'>" + moment(new Date()).add(1, "d").format("M/D/YY") + "</h5>"
+    futureDayInfoEl.innerHTML = "<h5 class='card-title text-dark'>" + moment(new Date()).add(1, "d").format("M/D/YY") + 
+    "<img class=img-fluid max-width: 10%; src=http://openweathermap.org/img/wn/" + futureDays[0].weather[0].icon + "@2x.png></img>"
+    + "</h5>"
     + "<p class='card-text text-dark'>" 
     + futureDays[0].temp.day + "°F<br>" 
     + futureDays[0].wind_speed + " MPH<br>"
@@ -128,7 +132,9 @@ var displayFutureDays = function(futureDays) {
 
     var futureDayTwoInfoEl = document.createElement("p");
     futureDayTwoInfoEl.classList = "card-title text-dark";
-    futureDayTwoInfoEl.innerHTML = "<h5 class='card-title text-dark'>" + moment(new Date()).add(2, "d").format("M/D/YY") + "</h5>"
+    futureDayTwoInfoEl.innerHTML = "<h5 class='card-title text-dark'>" + moment(new Date()).add(2, "d").format("M/D/YY") + 
+    "<img class=img-fluid max-width: 10%; src=http://openweathermap.org/img/wn/" + futureDays[1].weather[0].icon + "@2x.png></img>"
+    + "</h5>"
     + "<p class='card-text text-dark'>" 
     + futureDays[1].temp.day + "°F<br>" 
     + futureDays[1].wind_speed + " MPH<br>"
@@ -137,7 +143,9 @@ var displayFutureDays = function(futureDays) {
 
     var futureDayThreeInfoEl = document.createElement("p");
     futureDayThreeInfoEl.classList = "card-title text-dark";
-    futureDayThreeInfoEl.innerHTML = "<h5 class='card-title text-dark'>" + moment(new Date()).add(3, "d").format("M/D/YY") + "</h5>"
+    futureDayThreeInfoEl.innerHTML = "<h5 class='card-title text-dark'>" + moment(new Date()).add(3, "d").format("M/D/YY") + 
+    "<img class=img-fluid max-width: 10%; src=http://openweathermap.org/img/wn/" + futureDays[2].weather[0].icon + "@2x.png></img>"
+    + "</h5>"
     + "<p class='card-text text-dark'>" 
     + futureDays[2].temp.day + "°F<br>" 
     + futureDays[2].wind_speed + " MPH<br>"
@@ -146,7 +154,9 @@ var displayFutureDays = function(futureDays) {
 
     var futureDayFourInfoEl = document.createElement("p");
     futureDayFourInfoEl.classList = "card-title text-dark";
-    futureDayFourInfoEl.innerHTML = "<h5 class='card-title text-dark'>" + moment(new Date()).add(4, "d").format("M/D/YY") + "</h5>"
+    futureDayFourInfoEl.innerHTML = "<h5 class='card-title text-dark'>" + moment(new Date()).add(4, "d").format("M/D/YY") + 
+    "<img class=img-fluid max-width: 10%; src=http://openweathermap.org/img/wn/" + futureDays[3].weather[0].icon + "@2x.png></img>"
+    + "</h5>"
     + "<p class='card-text text-dark'>" 
     + futureDays[3].temp.day + "°F<br>" 
     + futureDays[3].wind_speed + " MPH<br>"
@@ -155,7 +165,9 @@ var displayFutureDays = function(futureDays) {
 
     var futureDayFiveInfoEl = document.createElement("p");
     futureDayFiveInfoEl.classList = "card-title text-dark";
-    futureDayFiveInfoEl.innerHTML = "<h5 class='card-title text-dark'>" + moment(new Date()).add(5, "d").format("M/D/YY") + "</h5>"
+    futureDayFiveInfoEl.innerHTML = "<h5 class='card-title text-dark'>" + moment(new Date()).add(5, "d").format("M/D/YY") + 
+    "<img class=img-fluid max-width: 10%; src=http://openweathermap.org/img/wn/" + futureDays[4].weather[0].icon + "@2x.png></img>"
+    + "</h5>"
     + "<p class='card-text text-dark'>" 
     + futureDays[4].temp.day + "°F<br>" 
     + futureDays[4].wind_speed + " MPH<br>"
